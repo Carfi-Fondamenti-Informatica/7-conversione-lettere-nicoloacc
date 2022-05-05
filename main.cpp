41da82b04a02e86a068225654b56e10171f0078f
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-05 10:35:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
 int main(){
+  char n;
+    cout << "inserisci un carattere" << endl;
+        cin >> n;
+        if (number (n) && (n<=90 && n>=65)) {
+        cout << char (n+32);}
+        else if (number (n) && (n>=97 && n<=122)) {
+        cout << char (n-32);}
+        else {
+        cout << "errore"; }
   return 0;
 }
