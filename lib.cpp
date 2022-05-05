bfbf515e474ce8f3d8d5d20703dfb45b1ef0c4bb
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-05 10:54:43 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 
-bool numb (int a) {
-    if ((a<=90 && a>=65)||(a>=97 && a<=122)) {
+bool numb (char a) {
+    if ((a>=65 && a<=90) || (a>=97 && a<=122)) {
         return true;
     }
     else {
