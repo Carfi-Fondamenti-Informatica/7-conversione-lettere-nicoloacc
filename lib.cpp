f1e35d734b44df351fee20e4ceb936b4d6bc0d0a
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-05 10:51:54 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,9 +2,9 @@
 
 bool numb (int a) {
     if ((a<=90 && a>=65)||(a>=97 && a<=122)) {
-        return 1;
+        return true;
     }
     else {
-        return 0;
+        return false;
          }
     }
