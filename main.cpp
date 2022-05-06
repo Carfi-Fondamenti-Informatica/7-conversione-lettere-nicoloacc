87303e2fd111dc6bbdce08e56a48a463ecf521c8
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-06 11:02:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+
+
+int main() {
+    char n;
+        cin >> n;
+        if (number (n) && (n<=90 && n>=65)) {
+        cout << char (n+32);}
+        else if (number (n) && (n>=97 && n<=122)) {
+        cout << char (n-32);}
+        else {
+        cout << "errore"; }
+    return 0;
+}
