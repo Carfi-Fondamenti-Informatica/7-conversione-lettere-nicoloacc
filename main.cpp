6f5f16746c1eae8717d73694bde3179e98c896ab
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-06 10:33:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include "lib.h"
-using namespace std;
 
-int main() {
-  char a;
-  cin >> a;
-  if(controllo(a)) {
-   cout<<a;
-  } else {
-      cout<<"errore";
-  }
-    return 0;
-}
