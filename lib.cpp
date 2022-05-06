50f66614ecfc35a945af6b892a634aae5f0fd9fb
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-06 09:56:04 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,7 +1,7 @@
 #include "lib.h"
 
 bool number (int a) {
-    if ((a<=90 && a>=65)|(a>=97 && a<=122)) {
+    if ((a<=90 && a>=65)||(a>=97 && a<=122)) {
         return true;
     } else {
         return false;
