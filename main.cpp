baaf03f75980dd77950d63f57a0682cf03e7b338
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-06 10:17:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int main() {
     char n;
         cin >> n;
-        if (carattere (n) && (n<=90 && n>=65)) {
+    bool x = carattere(n);
+        if ((x==1) && (n<=90 && n>=65)) {
         cout << char (n+32);}
-        else if (carattere (n) && (n>=97 && n<=122)) {
+        else if ((x==1) && (n>=97 && n<=122)) {
         cout << char (n-32);}
-        else {
+        else if (x==0) {
         cout << "errore"; }
     return 0;
 }
