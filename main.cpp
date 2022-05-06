ae1836613e179c2f6f968bb1a5338929d42b0dfe
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-06 10:24:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,12 @@
 using namespace std;
 
 int main() {
-    char n;
-        cin >> n;
-    bool x = conversione(n);
-        if ((x==1) && (n<=90 && n>=65)) {
-        cout << char (n+32);}
-        else if ((x==1) && (n>=97 && n<=122)) {
-        cout << char (n-32);}
-        else if (x==0) {
-        cout << "errore" << endl; }
+  char a;
+  cin >> a;
+  if(controllo(a)) {
+   cout<<a;
+  } else {
+      cout<<"errore";
+  }
     return 0;
 }
