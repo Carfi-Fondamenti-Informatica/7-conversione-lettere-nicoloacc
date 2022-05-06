dd7915d8bb8031b2f922c0e14c47d2a61ae353a5
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-06 10:07:47 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
   char n;
         cin >> n  ;
-        if (number (n) && (n<=90 && n>=65)) {
+        if (numb (n) && (n<=90 && n>=65)) {
         cout << char (n+32);}
         else if (number (n) && (n>=97 && n<=122)) {
         cout << char (n-32); }
