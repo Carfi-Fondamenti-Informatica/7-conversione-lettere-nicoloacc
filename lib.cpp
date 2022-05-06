d37a5e919b2849f0d5204f9b145ae471f89eabc6
nicoloacc <100343394+nicoloacc@users.noreply.github.com>
2022-05-06 10:33:48 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,17 +1,1 @@
-#include "lib.h"
-bool controllo(char & b)
-{
-    int a=(int)b;
-    if(a>=65 && a<=90) {
-        a=a+32;
-        b=(char)a;
-        return true;
-    }
-    else if(a>=97 && a<=122) {
-        a=a-32;
-        b=(char)a;
-        return true;
-    } else {
-        return false;
-    }
-}
+
